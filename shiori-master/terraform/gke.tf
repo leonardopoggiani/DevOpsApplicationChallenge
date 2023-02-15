061624bec4b624e3f81d15ce3499806f502d0b1e
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2023-02-15 11:29:15 +0100

    debugging CI/CD

--- shiori-master/terraform/gke.tf
+++ shiori-master/terraform/gke.tf
@@ -23,7 +23,6 @@ resource "google_container_cluster" "cluster" {
   remove_default_node_pool = true
 
   network    = google_compute_network.vpc.name
-  subnetwork = google_compute_subnetwork.subnet.name
 }
 
 resource "google_container_node_pool" "cluster_node_pool" {
--- shiori-master/terraform/vpc.tf
+++ shiori-master/terraform/vpc.tf
@@ -9,12 +9,5 @@ provider "google" {
 # VPC
 resource "google_compute_network" "vpc" {
   name                    = "${var.project_id}-vpc"
-  auto_create_subnetworks = "false"
-}
-
-# Subnet
-resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
+  auto_create_subnetworks = "true"
 }
